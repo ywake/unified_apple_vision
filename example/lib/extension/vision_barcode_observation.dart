3693138840b61ef9e57308375599692cb02032b3
ywake <ywake@student.42tokyo.jp>
2024-02-17 11:59:02 +0900

    Update: example

---
+++ example/lib/extension/vision_barcode_observation.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:unified_apple_vision/unified_apple_vision.dart';
+import 'package:unified_apple_vision_example/extension/vision_rectangle_observation.dart';
+
+extension VisionBarcodeObservationEx on VisionBarcodeObservation {
+  Widget build() {
+    return Positioned.fill(
+      child: CustomPaint(
+        painter: _Painter(this),
+      ),
+    );
+  }
+}
+
+class _Painter extends CustomPainter {
+  final VisionBarcodeObservation barcode;
+
+  _Painter(this.barcode);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    barcode.drawRect(canvas: canvas, size: size);
+
+    // Draw the text
+    final rect = barcode.scale(size).reverse(Offset(0, size.height));
+    final text = barcode.payloadStringValue;
+    final textPainter = TextPainter(
+      text: TextSpan(
+        text: text,
+        style: const TextStyle(fontSize: 10),
+      ),
+      textDirection: TextDirection.ltr,
+    )..layout();
+    textPainter.paint(canvas, rect.topLeft);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
+}
